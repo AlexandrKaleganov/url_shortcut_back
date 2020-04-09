f20d1ac2b5705d5b2380c395341d421c9554c700
Alexander Kaleganov <alexmur07@mail.ru>
2020-04-10 00:31:06 +0700

    jsp пашет

--- src/main/java/ru/akaleganov/job4j_url_shortcut/web/rest/IndexServlet.java
+++ src/main/java/ru/akaleganov/job4j_url_shortcut/web/rest/IndexServlet.java
@@ -3,7 +3,6 @@ package ru.akaleganov.job4j_url_shortcut.web.rest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
